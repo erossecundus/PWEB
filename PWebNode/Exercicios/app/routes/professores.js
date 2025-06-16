10180297705da3ac6ab98b474a7199eccaf09a54
EROS SECUNDUS ROCHA PINTERICH DO CANTO <0030482321001@lti.fatecso.edu.br>
2025-06-16 12:52:21 -0300

    update PwebNode: aula; rota detal_prof nao funcionando

--- PWebNode/Exercicios/app/routes/professores.js
+++ PWebNode/Exercicios/app/routes/professores.js
@@ -1,16 +1,24 @@
 // para acessar o config volta um nivel
-let dbConnection = require('../config/dbConnection');
+// let dbConnection = require('../config/dbConnection'); // foi carregado no server
 
 module.exports = function (app) {
     app.get('/informacao/professores', function (req, res) {
         async function getProfessores() {
             try {
-                const pool = await dbConnection(); // executa a funçao
-                const results = await pool.request().query('SELECT * from PROFESSORES');
+                let connection = app.config.dbConnection; // inclui pq dbConn vem do server
 
-                // res.json(results.recordset);
+                const pool = await connection(); //dbConnection(); // executa a funçao
+                //const results = await pool.request().query('SELECT * from PROFESSORES');
 
-                res.render('informacao/professores', { profs: results.recordset });
+                let professoresModel = app.models.professorModel; //vai recuperar a funcao get
+                                
+                // executar a função, passar a conexao e callback
+                professoresModel.getProfessores(pool, function(error, results) {
+                    res.render('informacao/professores', { profs: results.recordset });
+                })
+
+                //res.json(results.recordset);
+                //res.render('informacao/professores', { profs: results.recordset });
             } catch (err) {
                 console.log(err);
             }
